351b9a48fa9445725d79a5754571f9245efef0f8
Qs <quan.s.box@gmail.com>
2017-02-12 10:36:22 -0500

    add ROI to getHogWindows();

--- vehicle_tracking/FeatureExtractor_WindowPlanner.py
+++ vehicle_tracking/FeatureExtractor_WindowPlanner.py
@@ -23,7 +23,13 @@ def generateSlidingWindows(img_shape, rowcol_stride_pix, col_start_stop=[None, N
         col_start = col_start_stop[0]
         col_stop = col_start_stop[1]     
 
- 
+    assert row_start <= row_stop
+    assert col_start <= col_stop
+    assert row_stop <= img_shape[0]
+    assert col_stop <= img_shape[1]
+    
+    assert rowcol_window[0] <= img_shape[0], 'Expect search window being smaller than image. rowcol_window[0] = {}, img_shape[0] = {}'.format(rowcol_window[0], img_shape[0])
+    assert rowcol_window[1] <= img_shape[1], 'Expect search window being smaller than image. rowcol_window[0] = {}, img_shape[0] = {}'.format(rowcol_window[1], img_shape[1])
 
     # Compute the span of the region to be searched
     # Compute the number of pixels per step in col/row
@@ -53,18 +59,15 @@ def generateSlidingWindows(img_shape, rowcol_stride_pix, col_start_stop=[None, N
 
 
 class WindowPlanner():
-    def __init__(self, training_image_shape = (64,64), pix_per_cell = 8, cell_per_block = 2):
+    def __init__(self, training_image_shape = (64,64), pix_per_cell = 8):
 
         assert len(training_image_shape) == 2, 'Expect a width x length 2D shape, but training_image_shape = {} \n'. format(training_image_shape) 
         self.training_image_shape = training_image_shape
 
-        pix_per_blk_pos = pix_per_cell
-
-        self.pix_step = 8 
-
+        self.pix_step = pix_per_cell  # pixels of a block step
 
     # window size has to equal to training_image_shape
-    def getHogWindows(self, img):
+    def getHogWindows(self, img, region_of_interest_col_ratio=(0.6, 1) ):
         """eatch window corner is a hog cell corner. as Hog block moves by cell
         
         Args:
@@ -81,17 +84,22 @@ class WindowPlanner():
         if (img.shape[0] <= training_shape[0]) and (img.shape[1] <= training_shape[1]):
             if img.shape != training_shape:
                 img = cv2.resize(img, training_shape)
-            windows=   [  ((0,0), training_shape) ] # scan-window is over the whole image
+            windows =  [  ((0,0), training_shape) ] # scan-window is over the whole image
         else:
 
-            
 
-            print('//TODO: Find scan windows for a video frame')
+            #  sliding step has to be divisible by pix of a block step or pix_per_cell
+            ROI_min, RIO_max = region_of_interest_col_ratio
 
-            #  sliding step has to be divisable by pix_per_blk_pos or pix_per_cell
-
-
-            windows.append([])
+            row_start =  int( ROI_min *img.shape[0] )
+            row_stop =  int(RIO_max*img.shape[0])
+            col_start = 0 
+            col_stop = img.shape[1]
+            windows = generateSlidingWindows(   img_shape=img.shape, 
+                                                rowcol_stride_pix=(self.pix_step, self.pix_step ), 
+                                                col_start_stop=[col_start, col_stop], 
+                                                row_start_stop=[row_start, row_stop] , 
+                                                rowcol_window=self.training_image_shape)
 
         return windows
 
@@ -103,37 +111,6 @@ def main():
 
 
 
-    #####################################################
-    # Training Images
-    #####################################################
-    print('\n######################### Training Image Test ############################\n')
-    training_img_brg = cv2.imread('data/vehicles/GTI_Right/image0025.png')
-
-    windows_planner = WindowPlanner(training_img_brg.shape[:2])
-
-    windows = windows_planner.getHogWindows(training_img_brg)
-
-    print('Number of windows: ', len(windows))
-    assert(len(windows) == 1)
-
-    print('Shape of the first window :', windows[0])
-    assert(windows[0][0] == (0,0)) # upper left pos should be at 0
-    assert(windows[0][1] == training_img_brg.shape[:2]) #bottom right pos
-
-    #####################################################
-    # Video Frame
-    #####################################################
-    print('\n\n######################### Video Frame Test ############################ \n')
-    video_img_brg = cv2.imread('data/test_images/test6.jpg')
-
-    print('Video frame shape: ', video_img_brg.shape)
-
-    windows = windows_planner.getHogWindows(video_img_brg)
-
-    print('Number of windows: ', len(windows))
-    # assert(len(windows) == 1)
-
-    print('Shape of the first window :', windows[0])
 
     #####################################################
     # Module Test on generateSlidingWindows()
@@ -148,11 +125,27 @@ def main():
     row_stop = 720
     pix_per_cell = 8
     win_size = 64
+
+
+
     windows = generateSlidingWindows(   img_shape=(img_row, img_col), 
                                         rowcol_stride_pix=(pix_per_cell,pix_per_cell), 
                                         col_start_stop=[col_start, col_stop], 
                                         row_start_stop=[row_start, row_stop] , 
                                         rowcol_window=(win_size, win_size))
+
+
+
+    print('img_col = ', img_col)
+    print('img_row = ', img_row)
+    print('col_start = ', col_start)
+    print('col_stop = ', col_stop)
+    print('row_start = ', row_start)
+    print('row_stop = ', row_stop)
+    print('pix_per_cell = ', pix_per_cell)
+
+    print('win_size = ', win_size)
+
 
     w0 = windows[0]
     w1 = windows[1]
@@ -177,6 +170,13 @@ def main():
     print('row_step = ', row_step)
     print('col_step = ', col_step)
     assert (row_step+col_step) == pix_per_cell, "window moves a step either in column or row axis, not both"
+    print('Number of windows: ', len(windows))
+
+    row_window_positions = ((row_stop-row_start - win_size)//pix_per_cell) +1
+    col_window_positions = ((col_stop-col_start - win_size)//pix_per_cell) +1
+    expected_win_num =  row_window_positions* col_window_positions
+    assert len(windows) == expected_win_num, 'Expected window number = {}. Actual window number = {}. \n row_window_positions = {}, col_window_positions = {}.'.format(expected_win_num, len(windows), row_window_positions, col_window_positions)
+
 
     print('\n------- rectangular window test------- ')
     img_col = 1280
@@ -188,7 +188,7 @@ def main():
 
     row_step = 8
     col_step = 16
-    win_size = (100,200)
+    win_shape = (100,200)
 
     print('img_col = ', img_col)
     print('img_row = ', img_row)
@@ -198,13 +198,20 @@ def main():
     print('row_stop = ', row_stop)
     print('row_step = ', row_step)
     print('col_step = ', col_step)
-    print('win_size = ', win_size)
+    print('win_shape = ', win_shape)
 
     windows = generateSlidingWindows(   img_shape=(img_row, img_col), 
                                         rowcol_stride_pix=(row_step,col_step), 
                                         col_start_stop=[col_start, col_stop], 
                                         row_start_stop=[row_start, row_stop] , 
-                                        rowcol_window=(win_size[0], win_size[1]))
+                                        rowcol_window=win_shape)
+    print('Number of windows: ', len(windows))
+
+    row_window_positions = ((row_stop-row_start - win_shape[0])//row_step) +1
+    col_window_positions = ((col_stop-col_start - win_shape[1])//col_step) +1
+    expected_win_num =  row_window_positions* col_window_positions
+    assert len(windows) == expected_win_num, 'Expected window number = {}. Actual window number = {}. \n row_window_positions = {}, col_window_positions = {}.'.format(expected_win_num, len(windows), row_window_positions, col_window_positions)
+
 
     w0 = windows[0]
     w1 = windows[1]
@@ -219,10 +226,10 @@ def main():
     print('window rows: {}, {}'.format(w0_row, w1_row))
     print('window columns: {}, {}'.format(w0_col, w1_col))
 
-    assert w0_row == win_size[0]
-    assert w0_col == win_size[1]
-    assert w1_row == win_size[0]
-    assert w1_col == win_size[1]
+    assert w0_row == win_shape[0], 'w0_row = {}, win_shape[0]= {} '.format( w0_row,win_shape[0])
+    assert w0_col == win_shape[1], 'w0_col = {}, win_shape[1]= {} '.format( w0_col ,win_shape[1])
+    assert w1_row == win_shape[0], 'w1_row = {}, win_shape[0]= {} '.format( w1_row ,win_shape[0])
+    assert w1_col == win_shape[1], 'w1_col = {}, win_shape[1]= {} '.format( w1_col ,win_shape[1])
 
     w01_row_step = w1[0][0] - w0[0][0]
     w01_col_step = w1[0][1] - w0[0][1]
@@ -236,15 +243,17 @@ def main():
     print('\n------- rectangular window test - all windows covered ------- ')
     print('Number of windows: ', len(windows))
 
-    row_window_positions = ((row_stop-row_start - win_size[0])//row_step) +1
-    col_window_positions = ((col_stop-col_start - win_size[1])//col_step) +1
+    row_window_positions = ((row_stop-row_start - win_shape[0])//row_step) +1
+    col_window_positions = ((col_stop-col_start - win_shape[1])//col_step) +1
     expected_win_num =  row_window_positions* col_window_positions
 
 
     assert len(windows) == expected_win_num, 'Expected window number = {}. Actual window number = {}. \n row_window_positions = {}, col_window_positions = {}.'.format(expected_win_num, len(windows), row_window_positions, col_window_positions)
+
     is_init = True
     w0 = None
     for w in windows:
+
         if is_init:
             is_init = False
             w0 = w # w0 stores the last window
@@ -257,10 +266,10 @@ def main():
         w0_col = w0[1][1] - w0[0][1]
         w1_row = w1[1][0] - w1[0][0]
         w1_col = w1[1][1] - w1[0][1]
-        assert w0_row == win_size[0], 'past window - row_size = {}, win_size = {}'.format( w0_row,   win_size[0])
-        assert w0_col == win_size[1], 'past window - col_size = {}, win_size = {}'.format( w0_col,   win_size[1])
-        assert w1_row == win_size[0], 'new window - row_size = {}, win_size = {}'.format( w1_row,   win_size[0])
-        assert w1_col == win_size[1], 'new window - col_size = {}, win_size = {}'.format( w1_col,   win_size[1])
+        assert w0_row == win_shape[0], 'past window - row_size = {}, win_shape = {}'.format( w0_row,   win_shape[0])
+        assert w0_col == win_shape[1], 'past window - col_size = {}, win_shape = {}'.format( w0_col,   win_shape[1])
+        assert w1_row == win_shape[0], 'new window - row_size = {}, win_shape = {}'.format( w1_row,   win_shape[0])
+        assert w1_col == win_shape[1], 'new window - col_size = {}, win_shape = {}'.format( w1_col,   win_shape[1])
         w01_row_step = w1[0][0] - w0[0][0]
         w01_col_step = w1[0][1] - w0[0][1]
 
@@ -272,7 +281,122 @@ def main():
         w0 = w1 # w0 stores the last window
 
 
+    #####################################################
+    # Training Images
+    #####################################################
+    print('\n######################### Training Image Test ############################\n')
+    training_img_brg = cv2.imread('data/vehicles/GTI_Right/image0025.png')
 
+    pix_per_cell = 8
+    training_image_shape = training_img_brg.shape[:2]
+
+    windows_planner = WindowPlanner(training_image_shape=training_image_shape, pix_per_cell=pix_per_cell )
+
+    windows = windows_planner.getHogWindows(training_img_brg)
+
+    print('Number of windows: ', len(windows))
+    assert(len(windows) == 1)
+
+    print('Shape of the first window :', windows[0])
+    assert(windows[0][0] == (0,0)) # upper left pos should be at 0
+    assert(windows[0][1] == training_img_brg.shape[:2]) #bottom right pos
+
+
+
+    #####################################################
+    # Video Frame
+    #####################################################
+    print('\n\n######################### Video Frame Test ############################ \n')
+    video_img_brg = cv2.imread('data/test_images/test6.jpg')
+
+    ROI_min = 0.6
+    ROI_max = 0.9 
+    region_of_interest_col_ratio = (ROI_min, ROI_max)
+
+
+    
+    print('Video frame shape: ', video_img_brg.shape)
+    img_row = video_img_brg.shape[0]
+    img_col = video_img_brg.shape[1]
+
+    row_step = pix_per_cell
+    col_step = pix_per_cell
+    win_shape = training_image_shape
+
+    print('pix_per_cell = ', pix_per_cell)
+    print('region_of_interest_col_ratio = ', region_of_interest_col_ratio)
+    print('training_image_shape = ', training_image_shape)
+    print('img_col = ', img_col)
+    print('img_row = ', img_row)
+    print('row_step = ', row_step)
+    print('col_step = ', col_step)
+    print('win_shape = ', win_shape)
+
+    windows = windows_planner.getHogWindows(video_img_brg, region_of_interest_col_ratio)
+
+    # assert(len(windows) == 1)
+
+    print('The first window :', windows[0])
+    print('The last window :', windows[-1])
+
+    # test every window
+    is_init = True
+    w0 = None
+    for w in windows:
+
+        # test region of interest
+        upper = w[0]
+        lower = w[1]
+
+        lower_row = lower[0]
+        lower_col = lower[1]
+        assert (lower_row >= (ROI_min*img_row) ) and (lower_row<=ROI_max*img_row) , 'lower_row = {}, min = {}, max = {} '.format(lower_row,  ROI_min*img_row  , ROI_max*img_row)
+
+
+        if is_init:
+            is_init = False
+            w0 = w # w0 stores the last window
+            break
+
+        w1 = w
+
+
+        w0_row = w0[1][0] - w0[0][0]
+        w0_col = w0[1][1] - w0[0][1]
+        w1_row = w1[1][0] - w1[0][0]
+        w1_col = w1[1][1] - w1[0][1]
+
+        #test window shapes are the same as training shape
+        assert w0_row == win_shape[0], 'past window - row_size = {}, win_shape = {}'.format( w0_row,   win_shape[0])
+        assert w0_col == win_shape[1], 'past window - col_size = {}, win_shape = {}'.format( w0_col,   win_shape[1])
+        assert w1_row == win_shape[0], 'new window - row_size = {}, win_shape = {}'.format( w1_row,   win_shape[0])
+        assert w1_col == win_shape[1], 'new window - col_size = {}, win_shape = {}'.format( w1_col,   win_shape[1])
+        w01_row_step = w1[0][0] - w0[0][0]
+        w01_col_step = w1[0][1] - w0[0][1]
+
+        #test distance between windows
+        assert (w01_row_step == row_step ) or (w01_col_step==col_step), "window moves a step either in column or row axis"
+        assert ((w01_row_step == row_step ) and (w01_col_step==col_step) ) == False, "Expected: The window moves a step either in column or row axis. \
+                                                                                        Actual:  The window moves at both directions. \
+                                                                                        w01_row_step = {},  w01_col_step = {}".format(w01_row_step, w01_col_step)
+
+        w0 = w1 # w0 stores the last window
+
+
+    row_start = ROI_min*img_row
+    row_stop = ROI_max*img_row
+    print('row_start = ', row_start)
+    print('row_stop = ', row_stop)
+    col_start = 0
+    col_stop = img_col
+    row_window_positions = ((row_stop-row_start - win_shape[0])//pix_per_cell) +1
+    col_window_positions = ((col_stop-col_start - win_shape[1])//pix_per_cell) +1
+    expected_win_num =  row_window_positions* col_window_positions
+    print('Number of windows: ', len(windows))
+    assert len(windows) == expected_win_num, 'Expected window number = {}. Actual window number = {}. \n row_window_positions = {}, col_window_positions = {}.'.format(expected_win_num, len(windows), row_window_positions, col_window_positions)
+
+
+    print('\n**************** All Tests Passed! *******************\n')
 
 if __name__ == "__main__": 
     import time
